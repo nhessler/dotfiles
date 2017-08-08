b0af7c374e41e611b0e109123c061233243bc92f
Nathan Hessler <xmilestegx@gmail.com>
2017-08-07 21:29:35 -0700

    setup basic needs of bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
+#/ Bootstrap machine with dependencies
 
-echo "hello"
+set -e
+
+abort() { STEP="";   echo "!!! $*" >&2; exit 1; }
+log()   { STEP="$*"; echo "--> $*"; }
+logn()  { STEP="$*"; printf -- "--> %s " "$*"; }
+logk()  { STEP="";   echo "OK"; }
+
+cleanup() {
+  set +e
+
+  if [-s "$SUCCESS"]; then
+    if [ -n "$STEP" ]; then
+      echo "!!! $STEP FAILED" >&2
+    else
+      echo "!!! UNKNOWN FAILURE" >&2
+    fi
+  fi
+}
+
+trap "cleanup" EXIT
+
+SUCCESS="1"
+logk
+log "System Bootstrapped!"
