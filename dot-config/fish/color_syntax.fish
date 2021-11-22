a6574e7978b014aa4c2377b5ffe9ba3469a77ae3
Nathan Hessler <xmilestegx@gmail.com>
2021-11-22 10:55:56 -0500

    adding fish color syntax

---
+++ dot-config/fish/color_syntax.fish
@@ -0,0 +1,20 @@
+set -g fish_color_command green
+set -g fish_color_quote yellow
+set -g fish_color_redirection purple
+set -g fish_color_end brblack
+set -g fish_color_error red
+set -g fish_color_param cyan
+set -g fish_color_comment brblack
+set -g fish_color_match --background brblack
+set -g fish_color_selection --background brblack
+set -g fish_color_search_match --background white
+set -g fish_color_operator bryellow
+set -g fish_color_escape bryellow
+set -g fish_color_cwd brcyan
+set -g fish_color_autosuggestion brblack
+set -g fish_color_user brmagenta
+set -g fish_color_host yellow
+set -g fish_color_host_remote brred
+set -g fish_color_cancel bryellow
+
+# https://fishshell.com/docs/current/index#variables-for-changing-highlighting-colors
