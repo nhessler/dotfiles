457d0817ccc55c6ec46fcb62e9d57f0d23dafcae
Nathan Hessler <xmilestegx@gmail.com>
2023-04-07 21:37:00 -0500

    add some quality of life settings for emacs

--- emacs/prelude/personal/settings.el
+++ emacs/prelude/personal/settings.el
@@ -7,7 +7,15 @@
 
 (cua-mode t)
 
+;; Only spaces, no tabs
+(setq indent-tabs-mode nil)
+
+;; Always end a file with a newline
+(setq require-final-newline nil)
+
 (setq-default tab-width 2)
+(setq-default web-mode-markup-indent-offset 2)
+
 
 ;; theme
 
