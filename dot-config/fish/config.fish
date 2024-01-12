4026423ee70f0c631cd985c2d0ccea76831e98d7
Nathan Hessler <xmilestegx@gmail.com>
2024-01-12 09:37:55 -0600

    add direnv to the fish startup script

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -10,5 +10,19 @@ source ~/.config/fish/color_syntax.fish
 # asdf setup
 source ~/.asdf/asdf.fish
 
+# Enable IEx shell history
+# set -Ux ERL_AFLAGS '-kernel shell_history enabled'
+
+# Erlang config options
+# set -Ux KERL_CONFIGURE_OPTIONS '--without-javac --without-jinterface --without-odbc --without-hipe --with-wx-config=/opt/homebrew/bin/wx-config'
+# set -Ux KERL_BUILD_DOCS 'yes'
+
 # starship setup
 starship init fish | source
+
+# direnv setup
+direnv hook fish | source
+
+# set -g direnv_fish_mode eval_on_arrow    # trigger direnv at prompt, and on every arrow-based directory change (default)
+# set -g direnv_fish_mode eval_after_arrow # trigger direnv at prompt, and only after arrow-based directory changes before executing command
+# set -g direnv_fish_mode disable_arrow    # trigger direnv at prompt only, this is similar functionality to the original behavior
