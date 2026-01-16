0ecd8359defda145d63e9daa1256a86cf78005b8
Nathan Hessler <xmilestegx@gmail.com>
2026-01-15 21:08:48 -0600

    add completions for git

--- dot-config/fish/completions/git-secure.fish
+++ dot-config/fish/completions/git-secure.fish
@@ -8,7 +8,13 @@ complete -c git-secure -s e -l email -r -d "Author email for commits"
 complete -c git-secure -s k -l key -rF -d "Path to private signing key"
 complete -c git-secure -s f -l format -ra "ssh gpg" -d "Signature format (default: ssh)"
 
-- -r means the option requires an argument
+# Completions for git secure (subcommand)
+complete -c git -n "__fish_seen_subcommand_from secure" -s h -l help -d "Show help message"
+complete -c git -n "__fish_seen_subcommand_from secure" -s n -l name -r -d "Author name (default: global git user.name)"
+complete -c git -n "__fish_seen_subcommand_from secure" -s e -l email -r -d "Author email for commits"
+complete -c git -n "__fish_seen_subcommand_from secure" -s k -l key -rF -d "Path to private signing key"
+complete -c git -n "__fish_seen_subcommand_from secure" -s f -l format -ra "ssh gpg" -d "Signature format (default: ssh)"
+
 
 
 
